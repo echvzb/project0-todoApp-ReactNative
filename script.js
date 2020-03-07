0dab222a300e37743893dce0aee1a81718fc2b80
eduardochvzb <eduardochvzb@gmail.com>
2020-03-06 23:04:41 -0600

    Lista toDo

--- script.js
+++ script.js
@@ -9,6 +9,43 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+let unCheckedItems = 0
+let checkItems = 0
+let items = 0
+
+let done =()=>{
+  if(unCheckedItems==0) return
+  unCheckedItems--
+  checkItems++
+  itemCountSpan.innerHTML=checkItems
+  uncheckedCountSpan.innerHTML=unCheckedItems
+}
+
 function newTodo() {
-  alert('New TODO button clicked!')
+  let toDo = prompt('What are you going to do? :')
+  
+  if(toDo == null || toDo ==""){
+    alert('Invalid Input')
+    return
+  } 
+  else{
+    let item = document.createElement('li')
+    let box = document.createElement('input')
+
+    box.setAttribute("type","checkbox")
+    box.setAttribute("onclick", "done()")
+
+    item.setAttribute("class","todo-text")
+    item.appendChild(box)
+    item.innerHTML+=toDo
+    let id = "item-"+items
+    item.setAttribute("id",id)
+    items++
+
+    list.appendChild(item)
+    
+    unCheckedItems++
+    console.log(list)
+  }
+  uncheckedCountSpan.innerHTML=unCheckedItems
 }
